24f08753d3a0ae57c481baef21d38ee64fa95fc4
brli7 <146292564+brli7@users.noreply.github.com>
2025-04-09 11:51:06 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -13,7 +13,7 @@ public class OnlinePurchaseManager
   {
     int total = 0;
     for(int i = 0; i < purchases.size(); i++) {
-      if(purchases.get(i).isElectronic() && purchases.get(i).equals(maker)) {
+      if(purchases.get(i).isElectronic() && purchases.get(i).getMaker().equals(maker)) {
         total++;
       }
     }
